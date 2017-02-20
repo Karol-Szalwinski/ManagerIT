ba2a474406db5aa8b1a3f170141327218ae6c2e9
Karol-Szalwinski <karol.szalwinski@gmail.com>
2017-02-21 00:01:54 +0100

    Szablon początki

---
+++ src/ManagerITBundle/Resources/public/sidebar.php
@@ -0,0 +1,78 @@
+<div id="left">
+    <div class="media user-media bg-dark dker" id="medias">
+        <div class="user-media-toggleHover">
+            <span class="fa fa-user"></span>
+        </div>
+        <div class="user-wrapper bg-dark">
+            <a class="user-link" href="">
+                <img class="media-object img-thumbnail user-img" alt="User Picture" src="assets/img/user.gif">
+                <span class="label label-danger user-label">16</span>
+            </a>
+
+            <div class="media-body">
+                <h5 class="media-heading">Karol</h5>
+                <ul class="list-unstyled user-info">
+                    <li><a href="">Administrator</a></li>
+                    <li>Last Access : <br>
+                        <small><i class="fa fa-calendar"></i>&nbsp;1 Mar 16:32</small>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- #menu -->
+    <ul id="menu" class="bg-blue dker">
+        <li class="nav-header">Menu</li>
+        <li class="nav-divider"></li>
+
+        <li>
+            <a href="computers.php">
+                <i class="fa fa-desktop"></i>
+                <span class="link-title">Komputery</span>
+            </a>
+        </li>
+        <li>
+            <a href="laptops.php">
+                <i class="fa fa-laptop"></i>
+                <span class="link-title">Laptopy</span>
+            </a>
+        </li>
+        <li>
+            <a href="licenses.php">
+                <i class="fa fa-file-text"></i>
+                <span class="link-title">Licencje</span>
+            </a>
+        </li>
+        <li>
+            <a href="workers.php">
+                <i class="fa fa-id-card"></i>
+                <span class="link-title">Pracownicy</span>
+            </a>
+        </li>
+        <li>
+            <a href="users.php">
+                <i class="fa fa-user-o"></i>
+                <span class="link-title">Użytkownicy</span>
+            </a>
+        </li>
+        
+        <li>
+            <a href="departaments.php">
+                <i class="fa fa-building"></i>
+                <span class="link-title">Działy</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="login.html">
+                <i class="fa fa-sign-in"></i>
+                <span class="link-title">
+                    Login Page
+                </span>
+            </a>
+        </li>
+
+    </ul>
+    <!-- /#menu -->
+</div>
+

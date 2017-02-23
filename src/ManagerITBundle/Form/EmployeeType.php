636e6df9bff15da23d547f718e487898beb47428
Karol-Szalwinski <karol.szalwinski@gmail.com>
2017-02-24 00:23:50 +0100

    Formularze pracownika

--- src/ManagerITBundle/Form/EmployeeType.php
+++ src/ManagerITBundle/Form/EmployeeType.php
@@ -6,25 +6,30 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class EmployeeType extends AbstractType
-{
+class EmployeeType extends AbstractType {
+
     /**
      * {@inheritdoc}
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder->add('name')->add('surname')->add('sex')->add('phone')->add('email')
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('name')
+                ->add('surname')->add('sex', 'choice', array(
+                    'choices' => array(
+                        'Male' => 'Mężczyzna',
+                        'Female' => 'Kobieta',
+                    )
+                ))
+                ->add('phone')->add('email')
+                ->add('job')
                 ->add('departament', 'entity', [
                     'class' => 'ManagerITBundle:Departament',
                     'choice_label' => 'name']);
-                
     }
-    
+
     /**
      * {@inheritdoc}
      */
-    public function configureOptions(OptionsResolver $resolver)
-    {
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'ManagerITBundle\Entity\Employee'
         ));
@@ -33,10 +38,8 @@ class EmployeeType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getBlockPrefix()
-    {
+    public function getBlockPrefix() {
         return 'manageritbundle_employee';
     }
-
 
 }

98276dac35cfa5da53d7b417579c66687ed700e3
Karol <karol.szalwinski@gmail.com>
2017-04-26 15:34:45 +0200

    Widoki dla ssd wszystkie

--- src/ManagerITBundle/Form/SddType.php
+++ src/ManagerITBundle/Form/SddType.php
@@ -13,9 +13,49 @@ class SddType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')->add('model')->add('series')->add('brand')->add('capacity')->add('formFactor')->add('interface')        ;
+        $builder
+            ->add('name')
+            ->add('model')
+            ->add('series')
+            ->add('brand')
+            ->add('capacity', 'choice', array(
+                'choices' => array(
+                    '64' => '64 GB',
+                    '100' => '100 GB',
+                    '160' => '160 GB',
+                    '250' => '250 GB',
+                    '500' => '500 GB',
+                    '750' => '750 GB',
+                    '1024' => '1 TB',
+                    '2048' => '2 GB',
+                ),
+                'expanded' => false,
+                'multiple' => false,
+                'placeholder' => 'Wybierz pojemność',
+            ))
+            ->add('formFactor', 'choice', array(
+                'choices' => array(
+                    'PCIe card' => 'PCIe card',
+                    '2,5"' => '2,5"',
+                    'm.SATA' => 'm.SATA',
+                    'M.2' => 'M.2',
+                ),
+                'expanded' => false,
+                'multiple' => false,
+                'placeholder' => 'Wybierz obudowę',
+            ))
+            ->add('interface', 'choice', array(
+                'choices' => array(
+                    'SATA III' => 'SATA III',
+                    'SAS' => 'SAS',
+                    'PCIe' => 'PCIe',
+                ),
+                'expanded' => false,
+                'multiple' => false,
+                'placeholder' => 'Wybierz złącze',
+            ));
     }
-    
+
     /**
      * {@inheritdoc}
      */

68a22c8b2344d49cc81e79a6b4c12e5012c60f32
Karol <karol.szalwinski@gmail.com>
2017-06-01 14:44:43 +0200

    formularz dodawania nowej karty graficznej

--- src/ManagerITBundle/Form/GpuType.php
+++ src/ManagerITBundle/Form/GpuType.php
@@ -13,7 +13,75 @@ class GpuType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')->add('chipset')->add('series')->add('brand')->add('memorySize')->add('memoryType')->add('compatiableSlotPort')->add('cooling')->add('monitorConnectors')->add('powerConnectors')->add('formFactor')        ;
+        $builder->add('name')
+            ->add('chipset')
+            ->add('series')
+            ->add('brand')
+            ->add('memorySize')
+            ->add('memoryType', 'choice', array(
+                'choices' => array(
+                    'DDR2' => 'DDR2',
+                    'DDR3' => 'DDR3',
+                    'DDR5' => 'DDR5'
+                ),
+                'expanded' => false,
+                'multiple' => false,
+            ))
+            ->add('compatiableSlotPort', 'choice', array(
+                'choices' => array(
+                    'zintegrowana w cpu' => 'zintegrowana w cpu',
+                    'zintegrowana na płycie głównej' => 'zintegrowana na płycie głównej',
+                    'PCI' => 'PCI',
+                    'PCI Express x16' => 'PCI Express x16',
+                    'PCI Express x8' => 'PCI Express x8',
+                    'AGP' => 'AGP',
+                ),
+                'expanded' => false,
+                'multiple' => false,
+            ))
+            ->add('cooling', 'choice', array(
+                'choices' => array(
+                    'Aktywne' => 'aktywne ',
+                    'Pasywne' => 'pasywne ',
+                    'Nieoryginalne' => 'nieoryginalne '
+                ),
+                'expanded' => false,
+                'multiple' => false,
+            ))
+            ->add('monitorConnectors', 'choice', array(
+                'choices' => array(
+                    'HDMI ' => 'HDMI ',
+                    'DVI ' => 'DVI ',
+                    'Display Port ' => 'Display Port '
+                ),
+                'expanded' => true,
+                'multiple' => false,
+            ))
+
+            ->add('powerConnectors', 'choice', array(
+                'choices' => array(
+                    'brak' => 'brak',
+                    'PCIe 4 pin' => 'PCIe 4pin' ,
+                    'PCIe 6 pin' => 'PCIe 6pin',
+                    'PCIe 8 pin' => 'PCIe 8pin',
+                    'PCIe 6 + 6 pin' => 'PCIe 6 + 6 pin',
+                    'PCIe 6 + 8 pin' => 'PCIe 6 + 8 pin',
+                    'PCIe 8 + 8 pin' => 'PCIe 8 + 8 pin',
+                ),
+                'expanded' => false,
+                'multiple' => false,
+            ))
+            ->add('formFactor', 'choice', array(
+                'choices' => array(
+                    'karta  pci desktop' => 'karta  pci desktop ',
+                    'zintegrowana desktopowa' => 'zintegrowana desktopowa ',
+                    'zintegrowana laptop' => 'zintegrowana laptop ',
+                    'zewnętrzna usb' => 'zewnętrzna usb',
+                ),
+                'expanded' => false,
+                'multiple' => false,
+            ))
+        ;
     }
     
     /**

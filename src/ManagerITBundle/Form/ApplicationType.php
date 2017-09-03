c31972503575317be97c667182449cceea11f3a8
Karol-Szalwinski <karol.szalwinski@gmail.com>
2017-09-03 18:43:28 +0200

    crud aplikacji

---
+++ src/ManagerITBundle/Form/ApplicationType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ManagerITBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ApplicationType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('type')->add('licenseType')->add('developer')->add('webpage')->add('description')->add('addDate')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ManagerITBundle\Entity\Application'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'manageritbundle_application';
+    }
+
+
+}

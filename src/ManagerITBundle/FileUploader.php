79e1c7c1c38bc695968dad21a7174fe559f9210a
Karol-Szalwinski <karol.szalwinski@gmail.com>
2018-05-04 16:37:36 +0200

    Dodana obsługa miniaturek

--- src/ManagerITBundle/FileUploader.php
+++ src/ManagerITBundle/FileUploader.php
@@ -4,7 +4,8 @@ namespace ManagerITBundle;
 
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
-class FileUploader {
+class FileUploader
+{
     private $targetDir;
 
     public function __construct($targetDir)
@@ -14,9 +15,14 @@ class FileUploader {
 
     public function upload(UploadedFile $file)
     {
-        $fileName = md5(uniqid()) . '.' . $file->guessExtension();
+        $fileFirstName = md5(uniqid());
+        $fileExtension = '.' . $file->guessExtension();
+        $fileName = $fileFirstName . $fileExtension;
 
         $file->move($this->targetDir, $fileName);
+
+        $this->makeThumb($this->targetDir . "/" . $fileName, $this->targetDir . "/thumb40/" . $fileFirstName . "thumb40" . $fileExtension, 40);
+        $this->makeThumb($this->targetDir . "/" . $fileName, $this->targetDir . "/thumb200/" . $fileFirstName . "thumb200" . $fileExtension, 200);
 
         return $fileName;
     }
@@ -25,4 +31,26 @@ class FileUploader {
     {
         return $this->targetDir;
     }
+
+    private function makeThumb($src, $dest, $desired_width)
+    {
+
+        /* read the source image */
+        $source_image = imagecreatefromjpeg($src);
+        $width = imagesx($source_image);
+        $height = imagesy($source_image);
+
+        /* find the "desired height" of this thumbnail, relative to the desired width  */
+        $desired_height = floor($height * ($desired_width / $width));
+
+        /* create a new, "virtual" image */
+        $virtual_image = imagecreatetruecolor($desired_width, $desired_height);
+
+        /* copy source image at a resized size */
+        imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, $desired_width, $desired_height, $width, $height);
+
+        /* create the physical thumbnail image to its destination */
+        imagejpeg($virtual_image, $dest);
+    }
+
 }
